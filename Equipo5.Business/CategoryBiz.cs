e50622eefea34d54544e2d02960fa595c80d3953
ldm92 <leandro.maronas@outlook.com>
2021-05-16 21:04:12 -0300

    Add Create and List of Category.

---
+++ Equipo5.Business/CategoryBiz.cs
@@ -0,0 +1,37 @@
+﻿using Equipo5.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Equipo5.Data.Services;
+
+namespace Equipo5.Business
+{
+    public class CategoryBiz
+    {
+        public void Agregar(Category model)
+        {
+            var db = new BaseDataService<Category>();
+            db.Create(model);
+
+        }
+
+        public List<Category> Listar()
+        {
+            var db = new BaseDataService<Category>();
+            var lista = db.Get();
+            
+            return lista;
+        }
+
+        /*public int Buscar()
+        {
+            var db = new BaseDataService<Category>();
+            var id = db.Get();
+
+            return lista;
+        }*/
+
+    }
+}
---
+++ Equipo5.Site/Controllers/CategoryController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Equipo5.Business;
+using Equipo5.Entities.Models;
+
+namespace Equipo5.Site.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var biz = new CategoryBiz();
+            var model = biz.Listar();
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category model)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                var biz = new CategoryBiz();
+                biz.Agregar(model);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                // que pasa con el error -> Bitacora
+                Console.WriteLine(e);
+                return View(model);
+            }
+
+            /*
+            [HttpGet]
+            public ActionResult Edit(int id)
+            {
+                var biz = new CategoryBiz();
+                var model = biz.Buscar(id);
+                if (model == null)
+                    throw new Exception();
+
+                return View(model);
+            }*/
+
+        }
+    }
+}
